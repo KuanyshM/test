5b59e7ec3eb9a11fd23211ea4adc65b0dbad884c
Kuanysh <mkuanysh@mypost.kz>
2020-05-15 09:15:00 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<form action="http://localhost/gateway-ps/index.php/main/saveLog/?name=name.zip&type=log" method="POST">
+  Select image to upload:
+  <input type="file"><!-- 
+  <input type="text" name="name" value="name.zip" id="fileToUpload">
+  <input type="text" name="type" value="log"id="fileToUpload"> -->
+
+
+  <input type="submit" value="Upload Image" name="submit">
+</form>
